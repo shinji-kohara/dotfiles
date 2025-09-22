78c4f1fae2172c6cdbc99d2b9e38694eac24e4c2
shinji-kohara <shinji.kohara@molten.co.jp>
2025-09-22 10:01:52 +0900

    [nvim] transpalent background

--- files/.config/nvim/lua/config/cmd.lua
+++ files/.config/nvim/lua/config/cmd.lua
@@ -1,1 +1,8 @@
 vim.cmd[[colorscheme torte]]
+
+-- transpalent background
+vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC",    { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn",  { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr",      { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
